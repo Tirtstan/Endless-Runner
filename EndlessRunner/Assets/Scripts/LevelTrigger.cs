a4b36c9e5db4542bf758e0570570d82a9c1db9bf
Tirt <75492176+Tirtstan@users.noreply.github.com>
2024-03-13 22:58:08 +0200

    curvature thing

--- EndlessRunner/Assets/Scripts/LevelTrigger.cs
+++ EndlessRunner/Assets/Scripts/LevelTrigger.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class LevelTrigger : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject parent;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             LevelManager.Instance.SpawnObstacle(new Vector3(0, 0, 100));
-            Destroy(parent, 3);
+            Destroy(transform.parent.gameObject, 3);
         }
     }
 }
